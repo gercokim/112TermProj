166994ea73175a5467d5f92a52688ebf67bf23f3
popokun <gckim@andrew.cmu.edu>
2021-11-13 18:04:53 -0500

    Improved platform drawing

--- TP0.py
+++ TP0.py
@@ -12,6 +12,25 @@ def appStarted(app):
     app.isGameOver = False
     app.paused = False
     app.playerTimer = 28
+    app.ground = []
+    app.platforms = [(app.width/1.5+1.5*app.width/10, app.height/2.25, 
+                     app.width/2+1.5*app.width/10, app.height/2), 
+                     (app.width/1.5-app.width/5, app.height/2.25+app.height/5, 
+                     app.width/2-app.width/5, app.height/2+app.height/5),
+                     (app.width/1.5+app.width/2, app.height/2.25+app.height/5, 
+                     app.width/2+app.width/2, app.height/2+app.height/5)]
+    app.platformNum = len(app.platforms)
+    app.platformSpacing = 90
+
+# returns starting bounds of the player
+def playerBounds(app):
+    cx0, cy0 = (app.cx+app.r-app.width/3.5, app.cy+app.r+app.width/12)
+    cx1, cy1 = (app.cx-app.r-app.width/3.5, app.cy-app.r+app.width/12)
+    return (cx0, cy0, cx1, cy1)
+
+# returns the bounds of a given platform
+def platformBounds(app, platform):
+    return app.platforms[platform]
 
 # Checks if the player is at center
 def isPlayerCenter(app):
@@ -21,6 +40,8 @@ def isPlayerCenter(app):
         return True
 
 def keyPressed(app, event):
+    if checkCollision(app):
+        app.score += 1
     if not app.isGameOver and isPlayerCenter(app):
         if event.key == 'Left':
             app.scrollX -= 10
@@ -67,8 +88,8 @@ def doStep(app):
     #app.playerScrollx += app.playerTimer
     app.playerTimer -= 4
 
-#def checkCollision(app):
-
+def checkCollision(app):
+    return 
 
 def redrawAll(app, canvas):
     # Game started text
@@ -80,21 +101,25 @@ def redrawAll(app, canvas):
         canvas.create_text(app.width/2, app.height/5, text='Level Complete!', font='Didot 25 bold', fill='purple')
     
     # Basic map
-    platformX, platformY = 0, app.height
     canvas.create_rectangle(0, app.height-app.height/10, app.width, app.height, 
                             fill='green', outline='black')
-    
+   
     # Three platforms
-    canvas.create_rectangle(app.width/1.5+1.5*app.width/10-app.scrollX, app.height/2.25, 
-                            app.width/2+1.5*app.width/10-app.scrollX, app.height/2, 
-                            fill='black', outline='black')
-    canvas.create_rectangle(app.width/1.5-app.width/5-app.scrollX, app.height/2.25+app.height/5, 
-                            app.width/2-app.width/5-app.scrollX, app.height/2+app.height/5, 
-                            fill='black', outline='black')
-    canvas.create_rectangle(app.width/1.5+app.width/2-app.scrollX, app.height/2.25+app.height/5, 
-                            app.width/2+app.width/2-app.scrollX, app.height/2+app.height/5, 
-                            fill='black', outline='black')
-    
+    # canvas.create_rectangle(app.width/1.5+1.5*app.width/10-app.scrollX, app.height/2.25, 
+    #                         app.width/2+1.5*app.width/10-app.scrollX, app.height/2, 
+    #                         fill='black', outline='black')
+    # canvas.create_rectangle(app.width/1.5-app.width/5-app.scrollX, app.height/2.25+app.height/5, 
+    #                         app.width/2-app.width/5-app.scrollX, app.height/2+app.height/5, 
+    #                         fill='black', outline='black')
+    # canvas.create_rectangle(app.width/1.5+app.width/2-app.scrollX, app.height/2.25+app.height/5, 
+    #                         app.width/2+app.width/2-app.scrollX, app.height/2+app.height/5, 
+    #                         fill='black', outline='black')
+
+    # More efficient platform drawing
+    for platform in range(app.platformNum):
+        (x0, y0, x1, y1) = platformBounds(app, platform)
+        canvas.create_rectangle(x0-app.scrollX, y0, x1-app.scrollX, y1, fill='black', outline='black')
+
     # Final platform to end game
     canvas.create_rectangle(app.width/1.5+app.width-app.scrollX, app.height/2.25+app.height/2.2, 
                             app.width/2+app.width-app.scrollX, app.height/2+app.height/2.2, 
@@ -105,8 +130,9 @@ def redrawAll(app, canvas):
     canvas.create_text(app.width/2, app.height/20, text=f'gameMargin = {app.gameMargin}', font='Arial 15 bold', fill='black')
 
     # Temporary dot character
-    canvas.create_oval(app.cx+app.r-app.width/3.5+app.playerScrollx, app.cy+app.r+app.width/12+app.playerScrolly,
-                       app.cx-app.r-app.width/3.5+app.playerScrollx, app.cy-app.r+app.width/12+app.playerScrolly, 
+    (cx0, cy0, cx1, cy1) = playerBounds(app)
+    canvas.create_oval(cx0+app.playerScrollx, cy0+app.playerScrolly,
+                       cx1+app.playerScrollx, cy1+app.playerScrolly, 
                        fill='red', outline='black')
 
 runApp(width=600, height=300) 
